31659dc99a8ec2ff7c4bf9d4f73cad922ff557c8
kimpogi2015 <kimpogi2015@gmail.com>
2023-03-09 14:15:05 +0800

    fetch users added

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -10,9 +10,7 @@ const initialState = {
 };
 
 export const fetchUsers = createAsyncThunk("user/fetchUsers", () => {
-	return axios
-		.get("https://jsonplaceholder.typicode.com/users")
-		.then((res) => res.data.map((user) => user.id));
+	return axios.get("https://jsonplaceholder.typicode.com/users").then((res) => res.data);
 });
 
 //
@@ -24,11 +22,14 @@ const userSlice = createSlice({
 			state.loading = true;
 		});
 		builder.addCase(fetchUsers.fulfilled, (state, action) => {
-			(state.loading = false)((state.users = action.payload));
+			state.loading = false;
+			state.users = action.payload;
 			state.error = "";
 		});
 		builder.addCase(fetchUsers.rejected, (state, action) => {
-			(state.loading = false)((state.users = []))((state.error = action.error.message));
+			state.loading = false;
+			state.users = [];
+			state.error = action.error.message;
 		});
 	},
 });
